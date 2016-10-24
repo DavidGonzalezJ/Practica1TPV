2efd4bcabc73eb39b3b79ad77d3c7a2de3835e95
David González <davgon12@ucm.es>
2016-10-24 10:12:33 +0200

    Metodos load y draw de TexturaSDL

--- TexturasSDL.cpp
+++ TexturasSDL.cpp
@@ -9,3 +9,35 @@ TexturasSDL::TexturasSDL()
 TexturasSDL::~TexturasSDL()
 {
 }
+
+bool load(SDL_Renderer* pRender, std::string const& nombArch,SDL_Texture* & pTextura) {
+	//The image we will load es nombArch
+	//std::string imgName{ nombArch };
+
+	SDL_Surface* pTempSurface = nullptr;
+	bool success = true;  //Loading success flag
+
+						  //Load image
+	pTempSurface = /*nombArch.c_str();*/ /*SDL_LoadBMP*/IMG_Load(nombArch.c_str());  // si usamos string: bmpName.c_str() 
+	if (pTempSurface == nullptr) {
+		std::cout << "Unable to load image " << nombArch << "! \nSDL Error: " << SDL_GetError() << '\n';
+		success = false;
+	}
+	else {
+		pTextura = SDL_CreateTextureFromSurface(pRender, pTempSurface);
+		SDL_FreeSurface(pTempSurface);
+		success = pTextura != nullptr;
+	}
+	return success;
+}/*Carga la imagen del archivo. En caso de error lo muestra y devuelve false, y en otro caso genera la textura. Cuidado con la anterior textura. */
+
+void draw(SDL_Renderer* pRender, SDL_Rect const& rect,SDL_Texture* pTextura) {
+	//Clear the window to background color 
+	SDL_RenderClear(pRender);
+
+	// Draw objets 
+	SDL_RenderCopy(pRender, pTextura, nullptr /*Toda la textura*/, & rect /*En el rectángulo rect*/);  // nullptr, nullptr -> toda la textura en toda la ventana
+
+	//Show the window
+	SDL_RenderPresent(pRender);
+}//Dibuja toda la textura en el rectángulo rect.
--- TexturasSDL.h
+++ TexturasSDL.h
@@ -1,20 +1,24 @@
 #pragma once
 #include <SDL.h>
 #include <string>
+#include <iostream>
+#include "SDL_image.h"//Para poder utilizar el IMG_Load en el bool load
 class TexturasSDL
 {
 public:
 	TexturasSDL();
 	~TexturasSDL();
-	bool load(SDL_Renderer*, std::string const& nombArch);
-	void draw(SDL_Renderer*, SDL_Rect const& rect);
+	bool load(SDL_Renderer* pRender, std::string const& nombArch, SDL_Texture* & pTextura);
+	void draw(SDL_Renderer* pRender, SDL_Rect const& rect);
 private:
 	SDL_Texture* pTextura;
 	SDL_Rect globRect;
-	bool load(SDL_Renderer*, std::string const& nombArch);/*Carga la imagen del archivo. En caso de
+	//bool load(SDL_Renderer* pRender, std::string const& nombArch);
+	/*Carga la imagen del archivo. En caso de
 	error lo muestra y devuelve false, y en otro caso genera la textura. Cuidado con la anterior textura. */
-	void draw(SDL_Renderer*, SDL_Rect const& rect); //Dibuja toda la textura en el rectángulo rect.
-	/*bool load(SDL_Renderer*, string const& nombArch, SDL_Color const& colKey) : Una vez
+	//void draw(SDL_Renderer* pRender, SDL_Rect const& rect); //Dibuja toda la textura en el rectángulo rect.
+	//bool load(SDL_Renderer* pRender, std::string const& nombArch, SDL_Color const& colKey); 
+	/*: Una vez
 	cargado el archivo, y antes de generar la textura, pone el color colKey transparente.
 	Puedes definir una función static SDL_Surface* load(string nombreArchivo) para cargar el
 	archivo.*/
